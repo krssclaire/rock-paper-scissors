e508bd2379deb4c0e1dcada2fb16346b35533dc5
Kristel <kristelclaire.02@gmail.com>
2023-04-22 08:11:05 +0000

    Create function for Computer's choice
    Add function named getComputerChoice() that is supposed to return "Rock",
    "Paper" or "Scissors" randomly.

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Am I working?");
+/*
+let rock = 0;
+let paper = 1;
+let scissors = 2;
+*/
+let computerSelection;
+let playerSelection;
+
+
+function getComputerChoice() {
+    computerSelection = Math.floor(Math.random() * 3);
+
+    if (computerSelection === 0) {
+        console.log("Rock");
+    } else if (computerSelection === 1) {
+        console.log("Paper");
+    } else {
+        console.log("Scissors");
+    }
+
+    console.log(computerSelection);
+}
+
+
+
+getComputerChoice();

8286191fb93d183a45b4958ac85db4ec3c9b56a3
Kristel <kristelclaire.02@gmail.com>
2023-04-22 09:13:49 +0000

    Add more functions in a hopefully logic order
    Create a game() function containing all the functions:
     - getComputerChoice() for selecting rock, paper or scissors randomly;
     - getPlayer Selection

--- script.js
+++ script.js
@@ -3,24 +3,65 @@ let rock = 0;
 let paper = 1;
 let scissors = 2;
 */
-let computerSelection;
-let playerSelection;
 
+function game() {
+    function playRound(playerSelection, computerSelection) {
+        
+        function getComputerChoice() {
+            computerSelection = Math.floor(Math.random() * 3);
 
-function getComputerChoice() {
-    computerSelection = Math.floor(Math.random() * 3);
+            if (computerSelection === 0) {
+                console.log("Rock");
+            } else if (computerSelection === 1) {
+                console.log("Paper");
+            } else {
+                console.log("Scissors");
+            }
 
-    if (computerSelection === 0) {
-        console.log("Rock");
-    } else if (computerSelection === 1) {
-        console.log("Paper");
-    } else {
-        console.log("Scissors");
+            console.log(computerSelection);
+        }
+
+        function getPlayerSelection() {
+            playerSelection = prompt("Choose among Rock, Paper or Scissors", "");
+            playerSelection.toLowerCase();
+            
+            if (playerSelection === "rock") {
+                console.log("Rock");
+            } else if (playerSelection === "paper") {
+                console.log("Paper");
+            } else {
+                console.log("Scissors");
+            }
+        }
+        
+        getPlayerSelection();
+        getComputerChoice();
+
+        const WIN = "You won!";
+        const LOSE = "You lost!";
+        const TIE = "It's a tie";
+
+        if (
+            (playerSelection === "rock" && computerSelection === 2) ||
+            (playerSelection === "paper" && computerSelection === 0) ||
+            (playerSelection === "scissors" && computerSelection === 1)  
+            ) {
+            console.log(WIN);
+        } else if (
+            (computerSelection === 0 && playerSelection === "scissors") ||
+            (computerSelection === 1 && playerSelection === "rock") ||
+            (computerSelection === 2 && playerSelection === "paper") 
+            ) {
+            console.log(LOSE);
+        } else {
+            console.log(TIE);
+        }
     }
 
-    console.log(computerSelection);
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+    playRound();
 }
-
-
-
-getComputerChoice();
+console.log(game());

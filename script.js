96f4523886601ca7f1bc78e56b808dd617cd694e
Kristel <kristelclaire.02@gmail.com>
2023-04-23 10:50:25 +0100

    Add statements that tracks the general score
    Add if / else statements inside the playRound() function to track the
    general scores of both players (human and computer).
    
    To-do: Optimize code with loops and shorter if / else statements

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-/*
-let rock = 0;
-let paper = 1;
-let scissors = 2;
-*/
-
 function game() {
+
+    let playerScore = 0;
+    let computerScore = 0;
+
     function playRound(playerSelection, computerSelection) {
         
         function getComputerChoice() {
@@ -22,7 +20,7 @@ function game() {
         }
 
         function getPlayerChoice() {
-            playerSelection = prompt("Choose among Rock, Paper or Scissors", "");
+            playerSelection = prompt('Choose among Rock, Paper or Scissors and do not forget to open Web Console to see the result (ctrl + Shift + J)', '');
             playerSelection.toLowerCase();
             
             if (playerSelection === "rock") {
@@ -37,31 +35,68 @@ function game() {
         getPlayerChoice();
         getComputerChoice();
 
-        const WIN = "You won!";
-        const LOSE = "You lost!";
-        const TIE = "It's a tie";
+        let message;
+        let playerPoint = 0;
+        let computerPoint = 0;
 
         if (
             (playerSelection === "rock" && computerSelection === 2) ||
             (playerSelection === "paper" && computerSelection === 0) ||
             (playerSelection === "scissors" && computerSelection === 1)  
             ) {
-            console.log(WIN);
+            message = "You won!";
+            playerPoint += 1;
+            console.log(message);
+            console.log("Player pts: " + playerPoint);
+            console.log("Computer pts: " + computerPoint);
         } else if (
             (computerSelection === 0 && playerSelection === "scissors") ||
             (computerSelection === 1 && playerSelection === "rock") ||
             (computerSelection === 2 && playerSelection === "paper") 
             ) {
-            console.log(LOSE);
+            computerPoint += 1;
+            message = "You lost!";
+            console.log(message);
+            console.log("Player pts: " + playerPoint);
+            console.log("Computer pts: " + computerPoint);
         } else {
-            console.log(TIE);
+            message = "It's a tie";
+            playerPoint += 0;
+            computerPoint += 0;
+            console.log(message);
+            console.log("Player pts: " + playerPoint);
+            console.log("Computer pts: " + computerPoint);
         }
-    }
 
+        if (message === "You won!") {
+            playerScore += playerPoint;
+            computerScore += computerPoint;
+        } else if (message === "You lost!") {
+            playerScore += playerPoint;
+            computerScore += computerPoint;
+        } else {
+            playerScore += playerPoint;
+            computerScore += computerPoint;
+        }
+
+        console.log("Player Score: " + playerScore);
+        console.log("ComputerScore: " + computerScore); 
+        console.log("------------"); 
+    }
+    
     playRound();
     playRound();
     playRound();
     playRound();
     playRound();
-}
-console.log(game());
+
+    if (playerScore > computerScore) {
+        console.log("Player is the final winner!");
+    } else if (computerScore > playerScore) {
+        console.log("Computer is the final winner!");
+    } else {
+        console.log("Peace and love, you both won :)");
+    }
+ }
+
+game();

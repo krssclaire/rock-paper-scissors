f7a53e244c684a2937417caf4074ddbcd42dc715
Kristel <kristelclaire.02@gmail.com>
2023-05-04 15:32:41 +0100

    Add UI interactions and the logic behind each game
    Modify visually the choices of Computer every time a particular choice
    is made.
    Make the game stop when any player (or computer) reaches 5 points.

--- script.js
+++ script.js
@@ -1,59 +1,65 @@
 const displayedPlayerScore = document.querySelector('#player-score');
 const displayedComputerScore = document.querySelector('#computer-score');
-const playerRock = document.querySelector('#rock');
-const playerPaper = document.querySelector('#paper');
-const playerScissors = document.querySelector('#scissors');
 const computerRock = document.querySelector('#computerRock');
 const computerPaper = document.querySelector('#computerPaper');
 const computerScissors = document.querySelector('#computerScissors');
 const output = document.querySelector('.message');
 const btn = document.getElementsByTagName('button');
-const computerOptions = document.querySelectorAll('.computer div');
-
 let playerScore = 0;
 let computerScore = 0;
 
 const buttonClicked = e => {
     buttonId = e.target.id;
+    if (playerScore == 5 && playerScore > computerScore) {
+        output.textContent = 'Player is the final winner!';
+        output.textContent = 'Refresh the page for a new round';
+        button.removeEventListener('click', buttonClicked);
+    } else if (computerScore == 5 && computerScore > playerScore) {
+        output.textContent = 'Computer is the final winner!';
+        output.textContent = 'Refresh the page for a new round';
+        button.removeEventListener('click', buttonClicked);
+    } else if (computerScore == 5 && playerScore == 5) {
+        output.textContent = 'Peace and love, you both won :)';
+        output.textContent = 'Refresh the page for a new round';
+        button.removeEventListener('click', buttonClicked);
+    } 
     playRound();
 }
 
 for (let button of btn) {
     button.addEventListener('click', buttonClicked);
-
 }
 
 function playRound(playerSelection, computerSelection) {
     playerSelection = buttonId;
     computerSelection = Math.floor(Math.random() * 3);
 
-        if (playerSelection === 'rock') {
-            playerRock.style.backgroundColor = 'transparent';
-            playerRock.style.color = 'white';
-        } else if (playerSelection === 'paper') {
-            playerPaper.style.backgroundColor = 'transparent';
-            playerPaper.style.color = 'white';
-        } else if (playerSelection === 'scissors') {
-            playerScissors.style.backgroundColor = 'transparent';
-            playerScissors.style.color = 'white';
-        }
+    if (computerSelection === 0) {
+        computerSelection = 'rock';
+        computerRock.style.backgroundColor = 'transparent';
+        computerRock.style.color = 'white';
+        computerPaper.style.backgroundColor = 'whitesmoke';
+        computerPaper.style.color = 'black'; 
+        computerScissors.style.backgroundColor = 'whitesmoke';
+        computerScissors.style.color = 'black';
+    } else if (computerSelection === 1) {
+        computerSelection = 'paper';
+        computerPaper.style.backgroundColor = 'transparent';
+        computerPaper.style.color = 'white';
+        computerRock.style.backgroundColor = 'whitesmoke';
+        computerRock.style.color = 'black';
+        computerScissors.style.backgroundColor = 'whitesmoke';
+        computerScissors.style.color = 'black';
+    } else {
+        computerSelection = 'scissors';
+        computerScissors.style.backgroundColor = 'transparent';
+        computerScissors.style.color = 'white';
+        computerRock.style.backgroundColor = 'whitesmoke';
+        computerRock.style.color = 'black';
+        computerPaper.style.backgroundColor = 'whitesmoke';
+        computerPaper.style.color = 'black'; 
+    }
 
-        if (computerSelection === 0) {
-            computerSelection = 'rock';
-            computerRock.style.backgroundColor = 'transparent';
-            computerRock.style.color = 'white';
-        } else if (computerSelection === 1) {
-            computerSelection = 'paper';
-            computerPaper.style.backgroundColor = 'transparent';
-            computerPaper.style.color = 'white';
-        } else {
-            computerSelection = 'scissors';
-            computerScissors.style.backgroundColor = 'transparent';
-            computerScissors.style.color = 'white';
-        }
-
-        
-    let message;
     let playerPoint = 0;
     let computerPoint = 0;
 
@@ -62,22 +68,19 @@ function playRound(playerSelection, computerSelection) {
         (playerSelection === 'paper' && computerSelection === 'rock') ||
         (playerSelection === 'scissors' && computerSelection === 'paper')  
         ) {
-        message = `You won: ${playerSelection} beats ${computerSelection}`;
         playerPoint += 1;
-        output.textContent = message;
+        output.textContent = `Your point: ${playerSelection} beats ${computerSelection}`;
     } else if (
         (computerSelection === 'rock' && playerSelection === 'scissors') ||
         (computerSelection === 'paper' && playerSelection === 'rock') ||
         (computerSelection === 'scissors' && playerSelection === 'paper') 
         ) {
         computerPoint += 1;
-        message = `You lost: ${computerSelection} beats ${playerSelection}`;
-        output.textContent = message;
+        output.textContent = `Computer's point: ${computerSelection} beats ${playerSelection}`;
     } else {
-        message = 'It is a tie!';
         playerPoint += 0;
         computerPoint += 0;
-        output.textContent = message;
+        output.textContent = 'It is a tie!';
     }
 
     playerScore += playerPoint;
@@ -85,13 +88,4 @@ function playRound(playerSelection, computerSelection) {
 
     displayedPlayerScore.textContent = `Player: ${playerScore}`;
     displayedComputerScore.textContent = `Computer: ${computerScore}`;
-
 }
-
-if (playerScore == 5 && playerScore > computerScore) {
-    console.log('Player is the final winner!');
-} else if (computerScore == 5 && computerScore > playerScore) {
-    console.log('Computer is the final winner!');
-} else if (computerScore == 5 && playerScore == 5) {
-    console.log('Peace and love, you both won :)');
-} 

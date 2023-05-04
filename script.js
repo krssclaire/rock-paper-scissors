b2c6b866af6eb39be997c32ceceae5bf384eb5d1
Kristel <kristelclaire.02@gmail.com>
2023-05-04 14:20:20 +0100

    Add DOM interactions
    What was previously output on the console is now showed on the webpage
    thanks to DOM manipulation.

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
+const displayedPlayerScore = document.querySelector('#player-score');
+const displayedComputerScore = document.querySelector('#computer-score');
+const playerRock = document.querySelector('#rock');
+const playerPaper = document.querySelector('#paper');
+const playerScissors = document.querySelector('#scissors');
+const computerRock = document.querySelector('#computerRock');
+const computerPaper = document.querySelector('#computerPaper');
+const computerScissors = document.querySelector('#computerScissors');
+const output = document.querySelector('.message');
 const btn = document.getElementsByTagName('button');
+const computerOptions = document.querySelectorAll('.computer div');
+
+let playerScore = 0;
+let computerScore = 0;
 
 const buttonClicked = e => {
     buttonId = e.target.id;
@@ -6,102 +19,79 @@ const buttonClicked = e => {
 }
 
 for (let button of btn) {
-    button.addEventListener("click", buttonClicked);
-}
+    button.addEventListener('click', buttonClicked);
 
-let playerScore = 0;
-let computerScore = 0;
+}
 
 function playRound(playerSelection, computerSelection) {
     playerSelection = buttonId;
     computerSelection = Math.floor(Math.random() * 3);
 
-    function displayPlayerSelection() {
-        if (playerSelection === "rock") {
-            console.log("Rock");
-        } else if (playerSelection === "paper") {
-            console.log("Paper");
-        } else if (playerSelection === "scissors") {
-            console.log("Scissors");
+        if (playerSelection === 'rock') {
+            playerRock.style.backgroundColor = 'transparent';
+            playerRock.style.color = 'white';
+        } else if (playerSelection === 'paper') {
+            playerPaper.style.backgroundColor = 'transparent';
+            playerPaper.style.color = 'white';
+        } else if (playerSelection === 'scissors') {
+            playerScissors.style.backgroundColor = 'transparent';
+            playerScissors.style.color = 'white';
         }
-    }
 
-    function displayComputerSelection() {
         if (computerSelection === 0) {
-            console.log("Rock");
+            computerSelection = 'rock';
+            computerRock.style.backgroundColor = 'transparent';
+            computerRock.style.color = 'white';
         } else if (computerSelection === 1) {
-            console.log("Paper");
-        } else if (computerSelection === "scissors") {
-            console.log("Scissors");
+            computerSelection = 'paper';
+            computerPaper.style.backgroundColor = 'transparent';
+            computerPaper.style.color = 'white';
+        } else {
+            computerSelection = 'scissors';
+            computerScissors.style.backgroundColor = 'transparent';
+            computerScissors.style.color = 'white';
         }
+
         
-        console.log(computerSelection);
-    }
-
-    displayPlayerSelection();
-    displayComputerSelection();
-
-    
-    
-
-    
     let message;
     let playerPoint = 0;
     let computerPoint = 0;
 
     if (
-        (playerSelection === "rock" && computerSelection === 2) ||
-        (playerSelection === "paper" && computerSelection === 0) ||
-        (playerSelection === "scissors" && computerSelection === 1)  
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper')  
         ) {
-        message = "You won!";
+        message = `You won: ${playerSelection} beats ${computerSelection}`;
         playerPoint += 1;
-        console.log(message);
-        console.log("Player pts: " + playerPoint);
-        console.log("Computer pts: " + computerPoint);
+        output.textContent = message;
     } else if (
-        (computerSelection === 0 && playerSelection === "scissors") ||
-        (computerSelection === 1 && playerSelection === "rock") ||
-        (computerSelection === 2 && playerSelection === "paper") 
+        (computerSelection === 'rock' && playerSelection === 'scissors') ||
+        (computerSelection === 'paper' && playerSelection === 'rock') ||
+        (computerSelection === 'scissors' && playerSelection === 'paper') 
         ) {
         computerPoint += 1;
-        message = "You lost!";
-        console.log(message);
-        console.log("Player pts: " + playerPoint);
-        console.log("Computer pts: " + computerPoint);
+        message = `You lost: ${computerSelection} beats ${playerSelection}`;
+        output.textContent = message;
     } else {
-        message = "It's a tie";
+        message = 'It is a tie!';
         playerPoint += 0;
         computerPoint += 0;
-        console.log(message);
-        console.log("Player pts: " + playerPoint);
-        console.log("Computer pts: " + computerPoint);
+        output.textContent = message;
     }
 
-    if (message === "You won!") {
-        playerScore += playerPoint;
-        computerScore += computerPoint;
-    } else if (message === "You lost!") {
-        playerScore += playerPoint;
-        computerScore += computerPoint;
-    } else {
-        playerScore += playerPoint;
-        computerScore += computerPoint;
-    }
+    playerScore += playerPoint;
+    computerScore += computerPoint;
 
-    console.log("Player Score: " + playerScore);
-    console.log("ComputerScore: " + computerScore); 
-   
-    console.log("------------"); 
+    displayedPlayerScore.textContent = `Player: ${playerScore}`;
+    displayedComputerScore.textContent = `Computer: ${computerScore}`;
+
 }
 
-/*
-    if (playerScore > computerScore) {
-        console.log("Player is the final winner!");
-    } else if (computerScore > playerScore) {
-        console.log("Computer is the final winner!");
-    } else {
-        console.log("Peace and love, you both won :)");
-    }
- }
- */
+if (playerScore == 5 && playerScore > computerScore) {
+    console.log('Player is the final winner!');
+} else if (computerScore == 5 && computerScore > playerScore) {
+    console.log('Computer is the final winner!');
+} else if (computerScore == 5 && playerScore == 5) {
+    console.log('Peace and love, you both won :)');
+} 
